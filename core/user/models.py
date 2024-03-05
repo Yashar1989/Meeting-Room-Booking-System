8c3c981f7192ae7fa85f29f624717aa235e55e3f
Yashar1989 <yashar1989@gmail.com>
2024-03-05 16:39:21 +0100

    seperate signal in signals file

--- core/user/models.py
+++ core/user/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser 
 from phonenumber_field.modelfields import PhoneNumberField 
-from django.dispatch import receiver
-from django.db.models.signals import post_save
+
 # Create your models here. 
 
 class CustomUser(AbstractUser): 
@@ -13,7 +12,7 @@ class CustomUser(AbstractUser):
 
 
 class Profile(models.Model):
-    user = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE)
+    user = models.OneToOneField(to=CustomUser, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     image = models.ImageField(blank=True, null=True)
@@ -25,8 +24,3 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.email
 
-
-@receiver(signal=post_save, sender=CustomUser)
-def save_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
