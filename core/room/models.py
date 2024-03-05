568527367b5c40aa3df2503e7c831dd10ac6ff55
hesamgodarzi <hesamgodarzi@gmail.com>
2024-03-05 19:30:46 +0330

    create hesam model

--- core/room/models.py
+++ core/room/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+# create Reservation model
+class Reservation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
