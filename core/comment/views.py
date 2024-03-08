0338c2dbcb23f674f372c99ede5e25b6b61450f0
reza miraki <reza.miraki10@gmail.com>
2024-03-08 21:24:11 +0330

    fix: bug in add_comment

--- core/comment/views.py
+++ core/comment/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from comment.forms import CommentForm
 
 
@@ -8,11 +8,14 @@ from comment.forms import CommentForm
 @login_required
 def add_comment(request):
     if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.author_id = request.user.id
-            comment.save()
+        if request.user.is_authenticated:
+            form = CommentForm(request.POST)
+            if form.is_valid():
+                comment = form.save(commit=False)
+                comment.author_id = request.user.id
+                comment.save()
+        else:
+            return redirect(reverse('user:login'))
     else:
         form = CommentForm()
     return render(request, 'comment/add_comment.html', {'form': form})
