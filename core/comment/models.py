d38157c7c0b144276435f0d1a0c8d233cf8b6bb4
reza miraki <reza.miraki10@gmail.com>
2024-03-08 20:52:51 +0330

    feat: create comment form

--- core/comment/models.py
+++ core/comment/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from room.models import Reservation
 import uuid
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
 
 
 class Comment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    author = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
     reserve_id = models.ForeignKey(to=Reservation, on_delete=models.CASCADE)
     comment = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     parent = models.CharField(null=True, blank=True)
+    is_active = models.BooleanField(default=False, null=True)
--- core/comment/views.py
+++ core/comment/views.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from comment.forms import CommentForm
+
+
 # Create your views here.
+@login_required
+def add_comment(request):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+            form.author = request.user
+            form.save()
+    else:
+        form = CommentForm()
+    return render(request, 'comment/add_comment.html', {'form': form})
