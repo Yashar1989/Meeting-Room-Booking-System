4cdf86c48fd99331da18a5f63177aef75eb4ac89
PYMN <payman64love@yahoo.com>
2024-03-09 20:50:48 +0330

    complete rooms show available

--- core/room/views.py
+++ core/room/views.py
@@ -5,21 +5,4 @@ from .models import Room ,Reservation
 def room_availability(request):
     rooms = Room.objects.all()
     reservation = Reservation.objects.all()
-    # free_room = {}
-    # reserve_room = {}
-    # for room in rooms:
-    #     for reserve in reservation:
-    #         if room.id == reserve.room_id.id:
-    #             if reserve.start_time and reserve.end_time:
-    #                 reserve_start_time = reserve.start_time
-    #                 reserve_end_time = reserve.end_time
-    #                 if room.room_no not in reserve_room:
-    #                     reserve_room[room.room_no] = []
-    #                 reserve_room[room.room_no].append((reserve_start_time,reserve_end_time))
-    #             else:
-    #                 reserve_start_time = reserve.start_time
-    #                 reserve_end_time = reserve.end_time
-    #                 if room.room_no not in free_room:
-    #                     free_room[room.room_no] = []
-    #                 free_room[room.room_no].append((reserve_start_time,reserve_start_time))
     return render(request ,'room/room_availability.html',{'rooms':rooms ,'reservation':reservation})
