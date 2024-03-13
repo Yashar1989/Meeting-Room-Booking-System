9ab9c575264436bff5d8cbf47f8c5f77e14d6a6c
bagher <palahangmohammadbagher@gmail.com>
2024-03-14 03:16:08 +0330

    resolving confs

--- core/comment/models.py
+++ core/comment/models.py
@@ -9,9 +9,8 @@ from django.contrib.auth import get_user_model
 
 class Comment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    author = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
     reserve_id = models.ForeignKey(to=Reservation, on_delete=models.CASCADE)
     comment = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
-    parent = models.CharField(null=True, blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='replies')
     is_active = models.BooleanField(default=False, null=True)
