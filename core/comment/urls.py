3dea2c848e619ef26294ea3ee9233b5c0958c7dd
Yashar1989 <yashar1989@gmail.com>
2024-03-14 15:53:06 +0330

    Revert "Revert "Merge branch 'main' into bagher""
    This reverts commit bf15512907c7eb4eb4bea936ef54378eb86f4a69.

--- core/comment/urls.py
+++ core/comment/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add/', views.add_comment, name='add_comment'),
+    path('list-view/<uuid:pk>/', views.RoomCommentsListView.as_view(), name='comment_list_view'),
+    path('create/<uuid:pk>/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('update/<uuid:pk>/', views.CommentUpdateView.as_view(), name='comment_update'),
+    path('delete/<uuid:pk>/', views.CommentDeleteView.as_view(), name='comment_delete'),
+    path('admin/comments/', views.AdminCommentsView.as_view(), name='admin_comments'),
+    path('admin/comments/activate/', views.ActivateCommentsView.as_view(), name='activate_comments'),
+
+
 ]
