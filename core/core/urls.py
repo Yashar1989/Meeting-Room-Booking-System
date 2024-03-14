608f17b95972e504fccf9afbc420b629cf3bcdc8
Yashar1989 <yashar1989@gmail.com>
2024-03-14 15:30:06 +0330

    Revert "adding otp login"
    This reverts commit c9cec56fe36674290a460b8623f462bf3cd07e73.

--- core/core/urls.py
+++ core/core/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('user.urls')),
+    path('accounts/', include('user.urls'))
     path('comment/', include('comment.urls')),
     path('room/' ,include('room.urls')),
 ]
