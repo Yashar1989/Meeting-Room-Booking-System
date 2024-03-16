044ab7848cd405f51c2c75f467db54de73175457
Yashar1989 <yashar1989@gmail.com>
2024-03-16 20:49:31 +0330

    write new test for comment/room/user apps

--- core/comment/tests.py
+++ core/comment/tests.py
@@ -1,10 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse, resolve
 from .views import AdminCommentsView, ActivateCommentsView, CommentCreateView
+from .models import Comment
+from .forms import CommentForm
+from user.models import CustomUser
+from room.models import Room, Reservation
+from datetime import datetime
 
 
 class TestUrl(TestCase):
-
     def test_admin_comments_url(self):
         url = reverse('comment:admin_comments')
         self.assertEqual(resolve(url).func.view_class, AdminCommentsView)
@@ -16,3 +20,63 @@ class TestUrl(TestCase):
     def test_add_comment_url(self):
         url = reverse('comment:comment_create', kwargs={'pk': 'f3b9a1ef-5e16-45e9-8245-2184d5fe18ab'})
         self.assertEqual(resolve(url).func.view_class, CommentCreateView)
+
+
+class CommentFormTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.superuser = CustomUser.objects.create_superuser(username='admin1', password='!@#WSXqaz')
+        self.user = CustomUser.objects.create_user(username='yashar1', password='!@#WSXqaz')
+        self.room = Room.objects.create(room_no=2, capacity=100, description='test')
+        self.reserve = Reservation.objects.create(
+            user=self.user,
+            room_id=self.room.id,
+            reserve_date='2024-03-15',
+            available_time='8:00-10:00',
+            created_date=datetime.now()
+        )
+
+    def test_comment_form_with_valid_data(self):
+        """
+        create comment with CommentForm and validated data
+        """
+        form = CommentForm(data={
+            'reserve_id': self.reserve.id,
+            'user_id': self.user.id,
+            'comment': 'test',
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_comment_form_with_invalid_data(self):
+        """
+        create comment with CommentForm and validated data
+        """
+        form = CommentForm(data={
+        })
+        self.assertFalse(form.is_valid())
+
+
+class CommentModelTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.superuser = CustomUser.objects.create_superuser(username='admin2', password='!@#WSXqaz')
+        self.user = CustomUser.objects.create_user(username='yashar2', password='!@#WSXqaz')
+        self.room = Room.objects.create(room_no=3, capacity=100, description='test')
+        self.reserve = Reservation.objects.create(
+            user=self.user,
+            room_id=self.room.id,
+            reserve_date='2024-03-15',
+            available_time='8:00-10:00',
+            created_date=datetime.now()
+        )
+
+    def test_comment_model(self):
+        comment = Comment.objects.create(
+            user_id=self.user,
+            reserve_id=self.reserve,
+            comment='this is a comment',
+        )
+        self.assertEqual(comment.user_id.id, self.user.id)
+        self.assertEqual(comment.user_id.username, 'yashar2')
+        self.assertEqual(comment.reserve_id.id, self.reserve.id)
+        self.assertEqual(comment.comment, 'this is a comment')
--- core/room/tests.py
+++ core/room/tests.py
@@ -1,13 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse, resolve
 from .views import (
     RoomListView,
     ReservationListView,
     RoomCreate,
     RoomDetailView,
-    ReservationCreateView
+    ReservationCreateView,
+    room_availability
 )
 from .forms import RoomCreatingForm, ReservationForm
+from user.models import CustomUser
+from .models import Room, Reservation
+from datetime import datetime
 # Create your tests here.
 
 
@@ -47,6 +51,13 @@ class TestUrl(TestCase):
         url = reverse('room:reservation_create', kwargs={'room_no': 12})
         self.assertEqual(resolve(url).func.view_class, ReservationCreateView)
 
+    def test_room_availability_url(self):
+        """
+        check room_availability url view name
+        """
+        url = reverse('room:room_availability')
+        self.assertEqual(resolve(url).func, room_availability)
+
 
 class TestPostForm(TestCase):
     def test_create_room_with_valid_data(self):
@@ -71,3 +82,100 @@ class TestPostForm(TestCase):
             'available_time': '8:00-10:00'
         })
         self.assertTrue(form.is_valid())
+
+
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.superuser = CustomUser.objects.create_superuser(username='admin', password='!@#WSXqaz')
+        self.user = CustomUser.objects.create_user(username='yashar', password='!@#WSXqaz')
+        self.room = Room.objects.create(room_no=1, capacity=100, description='test')
+        self.reserve = Reservation.objects.create(
+            user=self.user,
+            room_id=self.room.id,
+            reserve_date='2024-03-15',
+            available_time='8:00-10:00',
+            created_date=datetime.now()
+        )
+
+    def test_room_index_url_get_response_200(self):
+        """
+        test index on project, list of rooms views
+        """
+        url = reverse('room:index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='room/index.html')
+
+    def test_add_room_url_non_superuser_response(self):
+        """
+        test add room views for non superuser
+        """
+        self.client.force_login(user=self.user)
+        # should be return 302 (redirect)
+        url = reverse('room:add_room')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_add_room_url_superuser_response(self):
+        """
+        test add room views for superuser
+        """
+        self.client.force_login(user=self.superuser)
+        # should be return 200 (redirect)
+        url = reverse('room:add_room')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_change_reserve_to_active_non_superuser_response(self):
+        """
+        test access to activate reservation for non superuser
+        """
+        self.client.force_login(user=self.user)
+        # should be return 302 (redirect)
+        url = reverse('room:active_reserve', kwargs={'reserve_id': self.reserve.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_change_reserve_to_active_superuser_response(self):
+        """
+        test access to activate reservation for superuser
+        """
+        self.client.force_login(user=self.superuser)
+        # should be return 200 (redirect)
+        url = reverse('room:active_reserve', kwargs={'reserve_id': self.reserve.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.resolver_match.route, '<str:reserve_id>/change/')
+
+
+class TestRoomModel(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.superuser = CustomUser.objects.create_superuser(username='admin4', password='!@#WSXqaz')
+        self.user = CustomUser.objects.create_user(username='yashar4', password='!@#WSXqaz')
+
+    def test_create_room(self):
+        room = Room.objects.create(
+            room_no=10,
+            capacity=20,
+            description='room number 10'
+        )
+        self.assertEqual(room.room_no, 10)
+        self.assertEqual(room.description, 'room number 10')
+
+    def test_create_reserve(self):
+        room = Room.objects.create(
+                        room_no=10,
+                        capacity=20,
+                        description='room number 10'
+                    )
+        reserve = Reservation.objects.create(
+            user=self.user,
+            room_id=room.id,
+            reserve_date='2024-03-15',
+            available_time='8:00-10:00',
+            created_date=datetime.now()
+        )
+        self.assertEqual(reserve.reserve_date, '2024-03-15')
+        self.assertEqual(reserve.available_time, '8:00-10:00')
